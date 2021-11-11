4fb5655c8bb5b3c248fd6bdb3ac806c6fe895469
John Kelly <60345872+jhkldev@users.noreply.github.com>
2021-11-11 10:56:15 +0000

    SASS-1637: Fix income tax submission build failing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "5.12.0",
-    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % "0.53.0",
+    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "5.16.0",
+    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % "0.56.0",
     "com.fasterxml.jackson.module"  %%  "jackson-module-scala"      % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                   %%  "bootstrap-test-play-28"     % "5.12.0"                 % Test,
+    "uk.gov.hmrc"                   %%  "bootstrap-test-play-28"     % "5.16.0"                 % Test,
     "org.scalatest"                 %%  "scalatest"                  % "3.2.9"                 % Test,
     "com.typesafe.play"             %%  "play-test"                  % current                 % Test,
     "org.pegdown"                   %   "pegdown"                    % "1.6.0"                 % "test, it",
@@ -19,7 +19,7 @@ object AppDependencies {
     "org.scalatestplus.play"        %%  "scalatestplus-play"         % "5.1.0"                 % "test, it",
     "com.github.tomakehurst"        %   "wiremock-jre8"              % "2.28.0"                % "test, it",
     "org.scalamock"                 %%  "scalamock"                  % "5.1.0"                 % Test,
-    "uk.gov.hmrc.mongo"             %%  "hmrc-mongo-test-play-28"    % "0.53.0"                % "test, it"
+    "uk.gov.hmrc.mongo"             %%  "hmrc-mongo-test-play-28"    % "0.56.0"                % "test, it"
   )
 
 }
