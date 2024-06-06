dbf4e1a9bd9d58f4bc38bf9ceae4eb999b83f1d9
ghousedev <49005870+ghousedev@users.noreply.github.com>
2024-06-06 11:56:29 +0100

    SASS-8421: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,8 +19,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "8.5.0"
-  private val mongoVersion = "1.8.0"
+  private val bootstrapPlay30Version = "8.6.0"
+  private val mongoVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapPlay30Version,
