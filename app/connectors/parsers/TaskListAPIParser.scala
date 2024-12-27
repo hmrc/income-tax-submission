6f09918cc0a6c64a5461a9be526910914cea8ee9
basotia <16934889+basotia@users.noreply.github.com>
2024-12-27 16:57:33 +0000

    generalised error handling

--- app/connectors/parsers/TaskListAPIParser.scala
+++ app/connectors/parsers/TaskListAPIParser.scala
@@ -24,6 +24,7 @@ import uk.gov.hmrc.http.{HttpReads, HttpResponse}
 import utils.PagerDutyHelper.PagerDutyKeys.UNEXPECTED_RESPONSE_FROM_API
 import utils.PagerDutyHelper.pagerDutyLog
 import play.api.http.Status._
+import uk.gov.hmrc.mongo.play.json.Codecs.logger
 import utils.PagerDutyHelper.PagerDutyKeys._
 
 trait TaskListAPIParser extends APIParser {
@@ -106,7 +107,7 @@ trait TaskListAPIParser extends APIParser {
         case SERVICE_UNAVAILABLE =>
           pagerDutyLog(SERVICE_UNAVAILABLE_FROM_API, logMessage(response))
           handleAPIError(response)
-        case _ =>
+        case e =>
           pagerDutyLog(UNEXPECTED_RESPONSE_FROM_API, logMessage(response))
           handleAPIError(response, Some(INTERNAL_SERVER_ERROR))
       }
